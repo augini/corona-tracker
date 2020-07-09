c94d50e865adbf49eb084531db7ee14e74d738e4
Farrukh Atabekov <augini18@gmail.com>
2020-07-10 02:21:16 +0900

    Updates

---
+++ precache-manifest.bf7a32820d8796f4513426996a7f25de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2a41e4df261afed21753eaab0ca3f8",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "079aed55553670171cee",
+    "url": "/corona-tracker/static/css/main.675036af.chunk.css"
+  },
+  {
+    "revision": "af969c0f6c57602939e9",
+    "url": "/corona-tracker/static/js/2.57836a79.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.57836a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079aed55553670171cee",
+    "url": "/corona-tracker/static/js/main.c884a717.chunk.js"
+  },
+  {
+    "revision": "7e7c08e4a6218267afe9",
+    "url": "/corona-tracker/static/js/runtime-main.1b83197d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
