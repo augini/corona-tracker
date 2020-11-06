ae60ae0bccd406f9ab4258972fce6c9c71b1046e
Farrukh <augini@inbox.ru>
2020-11-07 07:47:02 +0900

    fix: error

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import React, {Component} from 'react'
-import styles from './App.module.css'
-import {Cards, Chart, CountryPicker} from './components/'
-import { FetchData } from './api'
-import CoronaImage from './images/image.png'
-
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import { Cards, Chart, CountryPicker } from './components/';
+import { FetchData } from './api';
+import CoronaImage from './images/image.png';
 
 class App extends Component {
   state = {
-    data: ' '
-  }
+    data: ' ',
+  };
 
-  async componentDidMount(){
-    const data = await FetchData()
-    console.log(data);
-    this.setState({
-      data: data
-    })
-  }
-
-  handleChange  = async (country) => {
-    //fetch the data
-    const data = await FetchData(country)
+  async componentDidMount() {
+    const data = await FetchData();
+    // console.log(data);
     this.setState({
       data: data,
-      country: country
-    })
-    //set the state
-    console.log(country)
+    });
   }
 
+  handleChange = async (country) => {
+    //fetch the data
+    const data = await FetchData(country);
+    this.setState({
+      data: data,
+      country: country,
+    });
+    //set the state
+    console.log(country);
+  };
 
-  render() { 
-    const {data} =  this.state
-    return ( 
-      <div className = {styles.container}>
-        <Cards data = {data} />
-        <CountryPicker handleChange = {this.handleChange} />
-        <Chart data = {this.state.data} country = {this.state.country} />
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleChange={this.handleChange} />
+        <Chart data={this.state.data} country={this.state.country} />
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,45 +1,46 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const url = 'https://covid19.mathdro.id/api'
+const url = 'https://covid19.mathdro.id/api';
 
 export const FetchData = async (country) => {
   let changeableUrl = url;
 
-  if(country){
-    changeableUrl = `${url}/countries/${country}`
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
   }
 
   try {
-    const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableUrl)
-    return { confirmed, recovered, deaths, lastUpdate,}
-  }
-  catch (error) {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
 export const FetchDailyData = async () => {
   try {
-    const { data } = await axios.get(`${url}/daily`)
-
-    const modifiedData = data.slice(data.length-100).map((dailyData) => ({
-      confirmed: dailyData.confirmed.total,
-      deaths: dailyData.deaths.total,
-      date: dailyData.reportDate }))
-
-    return modifiedData
-  }
-  catch {
-
-  }
-}
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data
+      .slice(data.length - 100)
+      .map(({ confirmed, deaths, reportDate }) => ({
+        confirmed: confirmed.total,
+        deaths: deaths.total,
+        date: reportDate,
+      }));
+    console.log({ modifiedData });
+    return modifiedData;
+  } catch {}
+};
 
 export const FetchCountries = async () => {
   try {
-    const {data: {countries}} = await axios.get(`${url}/countries`)
-    return countries.map((country) => country.name)
-  }
-  catch(error) {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
     console.log(error);
   }
-}
+};
